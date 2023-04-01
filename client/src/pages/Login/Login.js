ff1acd71901b5666a56168d53ac81075ae9ac8b7
Ameera Noormohamed <ameera.noormohamed@divurgent.com>
2023-04-01 00:27:13 -0700

    completed log in page. added icons and added color changes

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
 import './style.css'
-import companyLogo from '../../components/images/logo.png';
 import Header from '../../components/Header';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser } from '@fortawesome/free-regular-svg-icons'
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
+import 'font-awesome/css/font-awesome.min.css';
 
 import Auth from '../../utils/auth';
 
@@ -47,18 +49,15 @@ const Login = (props) => {
   return (
     <div className="background flex flex-col justify-center">
       <Header />
-      {/* <div className="companyLogo flex flex-row justify-center">
-        <img src={companyLogo} alt="logo" />
-      </div> */}
       <main className="flex flex-col justify-stretch">
         <div className="card flex flex-row columns-2 h-full">
           <div className="leftColumn flex flex-col h-full w-2/6">
-            <div className="leftSide login h-full"><p>Login</p></div>
-            <div className="leftSide signup h-full"><p>Sign Up</p></div>
+            <div className="leftSide login h-full flex flex-col justify-center"><FontAwesomeIcon size='2x' icon={faCircleUser} /><div className="mx-auto pt-2">Log in</div></div>
+            <div className="leftSide signup h-full flex flex-col justify-center"><FontAwesomeIcon size='2x' icon={faPenToSquare} className="pl-2" /><div className="mx-auto pt-2">Sign up</div></div>
           </div>
 
           <div className="card-body flex flex-col w-full">
-            <div className="title bg-dark text-light p-2 h-1/6 text-center">Login</div>
+            <div className="title bg-dark text-light p-2 h-1/6 text-center">Log in here</div>
 
             {data ? (
               <p>
@@ -70,7 +69,7 @@ const Login = (props) => {
                 <div className="inputFields flex flex-col justify-center h-max pt-5">
                     <input
                       className="form-input w-5/6"
-                      placeholder="Your email"
+                      placeholder="&#xf0e0;  Email"
                       name="email"
                       type="email"
                       value={formState.email}
@@ -78,7 +77,7 @@ const Login = (props) => {
                     />
                     <input
                       className="form-input w-5/6 mt-5"
-                      placeholder="******"
+                      placeholder="&#xf084;  Password"
                       name="password"
                       type="password"
                       value={formState.password}
@@ -89,7 +88,7 @@ const Login = (props) => {
                   className="submit w-5/6"
                   style={{ cursor: 'pointer' }}
                 >
-                  Login
+                  Log in
                 </button>
               </form>
             )}
