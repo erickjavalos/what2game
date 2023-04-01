feaa05ecd55f61bcb84c2f32afd0ae5250aaa97c
eavalo14 <avaloserick97@gmail.com>
2023-03-31 19:50:48 -0700

    recovering...

--- client/src/components/config/config.js
+++
@@ -1,1 +0,0 @@
-export const API_KEY = 'your_api_key_here';
--- client/src/utils/rawgApi.js
+++
@@ -1,9 +0,0 @@
-// utils/rawgApi.js
-
-const API_BASE_URL = 'https://api.rawg.io/api/games';
-
-export const fetchData = async (query) => {
-  const response = await fetch(`${API_BASE_URL}?${query}`);
-  const data = await response.json();
-  return data.results;
-};
