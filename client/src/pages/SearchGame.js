833c2f4fca9014d7432e3a9b5ed7485046899b41
Andy <a.garcia.atn31@gmail.com>
2023-04-02 01:23:23 -0700

    search

---
+++ client/src/pages/SearchGame.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchGame from '../components/SearchGame/index';
+
+const SearchGamePage = () => {
+  return (
+    <div>
+      <SearchGame />
+    </div>
+  );
+};
+
+export default SearchGamePage;
