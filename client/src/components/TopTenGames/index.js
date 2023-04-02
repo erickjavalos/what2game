db3c3a379dfc19ca0e9d2e32218d602eb3307776
eavalo14 <avaloserick97@gmail.com>
2023-04-02 16:59:12 -0700

    fixed bug in linking

--- client/src/components/TopTenGames/index.js
+++ client/src/components/TopTenGames/index.js
@@ -14,13 +14,13 @@ const TopTenGames = ({ games = [] }) => {
         <div className='flex flex-wrap justify-center'>
         {games &&
             games.map((game) => (
-            <div key={game.id}className="w-full md:w-1/3 lg:w-1/4 p-3 text-center m-1">
+            <div key={game.id} className="w-full md:w-1/3 lg:w-1/4 p-3 text-center m-1">
                 <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-full flex flex-col hover:bg-[rgb(186,215,242)]">
                     <a href={'/asset?idgb_id=' + game.igdb_id} className="mt-2 flex items-center justify-center">
                         <img className="w-24 h-24 rounded-full shadow-lg" src={game.box_art_url} alt="game art"/>
                     </a>
                     <div className="flex-grow">
-                        <a href="#" className="h-full flex items-center">
+                        <a href={'/asset?idgb_id=' + game.igdb_id} className="h-full flex items-center">
                             <h5 className="mx-2 mb-6 mt-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{game.name}</h5>
                         </a>
                     </div>
