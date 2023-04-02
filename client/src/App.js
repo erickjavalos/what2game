e793c80153181313a99404bc32ddcce21303ee8a
FrankM89 <114839168+FrankM89@users.noreply.github.com>
2023-04-02 01:15:25 -0700

    Genres, gamedetails, mutations, queries, proxy server, apikey

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,42 @@
-import React from 'react';
+import React from "react";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import SingleThought from './pages/SingleThought';
-import Profile from './pages/Profile';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import GameDetails from './components/GameDetails';
-import GameReviews from './components/GameReviews';
-import Genres from './components/Genres';
-import SearchGame from './components/SearchGame';
-import Streams from './components/Streams';
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import GameDetails from "./components/GameDetails";
+import GameReviews from "./components/GameReviews";
+import Genres from "./components/Genres";
+import SearchGame from "./components/SearchGame";
+import Streams from "./components/Streams";
 
-import './dist/output.css'
-
+import "./dist/output.css";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -54,61 +53,25 @@ function App() {
       <Router>
         <div className="flex flex-col">
           <Header />
-          {/* <div className="container">
+          <div className="container">
             <Routes>
-              <Route 
-                path="/"
-                element={<Home />}
-              />
-              <Route 
-                path="/login" 
-                element={<Login />}
-              />
-              <Route 
-                path="/signup" 
-                element={<Signup />}
-              />
-              <Route 
-                path="/me" 
-                element={<Profile />}
-              />
-              <Route 
-                path="/profiles/:username" 
-                element={<Profile />}
-              />
-              <Route 
-                path="/thoughts/:thoughtId" 
-                element={<SingleThought />}
-              />
-              <Route 
-                path="/game/:gameId" 
-                element={<GameDetails />}
-              />
-              <Route 
-                path="/game/:gameId/reviews" 
-                element={<GameReviews />}
-              />
-              <Route 
-                path="/genres" 
-                element={<Genres />}
-              />
-              <Route 
-                path="/search" 
-                element={<SearchGame />}
-              />
-              <Route 
-                path="/streams" 
-                element={<Streams />}
-              />
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/me" element={<Profile />} />
+              <Route path="/profiles/:username" element={<Profile />} />
+              <Route path="/thoughts/:thoughtId" element={<SingleThought />} />
+              <Route path="/game/:gameSlug" element={<GameDetails />} />
+              <Route path="/game/:gameSlug/reviews" element={<GameReviews />} />
+              <Route path="/genres" element={<Genres />} />
+              <Route path="/search" element={<SearchGame />} />
+              <Route path="/streams" element={<Streams />} />
             </Routes>
           </div>
-          <Footer /> */}
+          <Footer />
         </div>
-
       </Router>
     </ApolloProvider>
-  
-   
   );
 }
 
