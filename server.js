e6d35aa8f0734ef3d1c94f9e11bcc0c576302cdf
eavalo14 <avaloserick97@gmail.com>
2023-04-05 01:47:15 -0700

    adding updates to code

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+const buildPath = path.join(__dirname, 'build');
+
+
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
-
+app.use(express.static(buildPath));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -21,9 +24,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
+app.get("/*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, buildPath, 'index.html'));
+})
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
@@ -41,3 +44,6 @@ const startApolloServer = async (typeDefs, resolvers) => {
   
 // Call the async function to start the server
   startApolloServer(typeDefs, resolvers);
+
+
+
