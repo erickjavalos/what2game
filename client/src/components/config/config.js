6a27aa42b110388bee3690d12e462804ffdc5900
eavalo14 <avaloserick97@gmail.com>
2023-04-05 03:12:06 -0700

    deploying

--- client/src/components/config/config.js
+++
@@ -1,1 +0,0 @@
-export const API_KEY = 'df0a6dbf13504aefb411f7298892a149';
---
+++ config/connection.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/your-database-name');
+module.exports = mongoose.connection;
