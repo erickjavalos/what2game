bfbff7e0ea9208bf3eddad7c7602feca033b3812
eavalo14 <avaloserick97@gmail.com>
2023-04-05 02:57:17 -0700

    small updates

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+require('dotenv').config()
+const buildPath = path.join(__dirname, 'build');
+
+
+
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -14,15 +19,13 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
+app.use(express.static(buildPath));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
 });
 
 
